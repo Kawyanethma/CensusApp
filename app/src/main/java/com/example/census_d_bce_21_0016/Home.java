86a9a707b1dfc83e440d6bc185172b2ff768afe3
Kawyanethma Walisundara <kawyanethma@gmail.com>
2023-04-13 23:59:08 +0530

    log in page and register page

---
+++ app/src/main/java/com/example/census_d_bce_21_0016/Home.java
@@ -0,0 +1,13 @@
+package com.example.census_d_bce_21_0016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
